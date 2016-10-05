fb533e8664a1a42e06943686209a8b138a6003ed
HakShak <oss@hakshak.com>
2016-10-05 23:24:29 +0000

    Added list command for controls

--- cmd/controls.go
+++ cmd/controls.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
 )
@@ -29,8 +32,66 @@ to quickly create a Cobra application.`,
 	},
 }
 
+var listControlsCmd = &cobra.Command{
+	Use:   "controls",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		controls, err := mamexml.LoadControlsXml("controls.xml")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		controlTypes := make(map[string][]string)
+
+		for _, game := range controls {
+			for _, player := range game.PlayerControls {
+				for _, control := range player.Controls {
+					if _, ok := controlTypes[control.Constant.Name]; !ok {
+						controlTypes[control.Constant.Name] = []string{control.Name}
+					} else {
+						descriptions := controlTypes[control.Constant.Name]
+						recorded := false
+						for _, description := range descriptions {
+							if control.Name == description {
+								recorded = true
+							}
+						}
+
+						if recorded {
+							break
+						}
+
+						controlTypes[control.Constant.Name] = append(descriptions, control.Name)
+					}
+				}
+			}
+		}
+
+		tw := new(tabwriter.Writer)
+		tw.Init(os.Stdout, 0, 0, 2, ' ', 0)
+		fmt.Fprintln(tw, "Keyword\tDescription")
+		fmt.Fprintln(tw, "-------\t-----------")
+
+		for keyword, descriptions := range controlTypes {
+			for _, description := range descriptions {
+				fmt.Fprintf(tw, "%s\t%v\n", keyword, description)
+			}
+		}
+
+		fmt.Fprintln(tw)
+		tw.Flush()
+	},
+}
+
 func init() {
 	statCmd.AddCommand(controlsCmd)
+	listCmd.AddCommand(listControlsCmd)
 
 	// Here you will define your flags and configuration settings.
 
--- mamexml/controls.go
+++ mamexml/controls.go
@@ -10,15 +10,29 @@ import (
 
 import "gopkg.in/cheggaaa/pb.v1"
 
+type ControlsConstant struct {
+	Name string `xml:"name,attr"`
+}
+
+type ControlsControl struct {
+	Name     string           `xml:"name,attr"`
+	Constant ControlsConstant `xml:"constant"`
+}
+
+type ControlsPlayer struct {
+	Controls []ControlsControl `xml:"controls>control"`
+}
+
 type ControlsGame struct {
-	RomName     string `xml:"romname,attr"`
-	GameName    string `xml:"gamename,attr"`
-	Players     int    `xml:"numPlayers,attr"`
-	Alternating bool   `xml:"alternating,attr"`
-	Mirrored    bool   `xml:"mirrored,attr"`
-	UsesService bool   `xml:"usesService,attr"`
-	Tilt        bool   `xml:"tilt,attr"`
-	Cocktail    bool   `xml:"cocktail,attr"`
+	RomName        string           `xml:"romname,attr"`
+	GameName       string           `xml:"gamename,attr"`
+	Players        int              `xml:"numPlayers,attr"`
+	Alternating    bool             `xml:"alternating,attr"`
+	Mirrored       bool             `xml:"mirrored,attr"`
+	UsesService    bool             `xml:"usesService,attr"`
+	Tilt           bool             `xml:"tilt,attr"`
+	Cocktail       bool             `xml:"cocktail,attr"`
+	PlayerControls []ControlsPlayer `xml:"player"`
 }
 
 func LoadControlsXml(filename string) (map[string]ControlsGame, error) {
